51bf161c36292228ec8b11e8e19979a746abbebe
gokhakan <71521573+gokhakan@users.noreply.github.com>
2026-09-14 10:21:06 +0100

    add for comrehension

--- src/main/scala/com/hakangok/FunctionalProgramming.scala
+++ src/main/scala/com/hakangok/FunctionalProgramming.scala
@@ -9,10 +9,29 @@ object FunctionalProgramming extends App{
   println(s"Double of $n is ${doubler(20)}")
 
   //higher order functions
+  //map
   val originalList = List(1,2,3)
   val aMappedList = originalList.map(x=>x*3)
   println("Mapped new list of: " +originalList + " is: " + aMappedList)
 
+  //flatmap
   val aFlatMap = List(1,2,3).flatMap(x=>List(x, x+10))
   println("Flatmapped new list: " + aFlatMap)
+
+  //filter
+  val aFilteredList = originalList.filter(x=>x<=2)
+  println("Filtered list: " + aFilteredList)
+
+  //all pair
+  val letterList =List('a','b','c')
+
+  val allPairs = originalList.flatMap(number=> letterList.map(letter=> s"$number-$letter"))
+  println("Allpairs: " + allPairs)
+
+  //for comprehensions
+  val alternativePair = for{
+    number<- originalList
+    letter<- letterList
+  }yield s"$number+$letter"
+  println("For comprehension result list: " + alternativePair)
 }
